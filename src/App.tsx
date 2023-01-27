5eb6f4f1a7f9247aea0546c4e5258dd9f5e18d35
Koray-K <kurucankoray@gmail.com>
2023-01-27 18:25:32 +0300

    Create Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
+import Card from './components/Card';
+
 function App() {
   return (
     <SafeAreaView>
       <View>
         <Text>Some Text</Text>
       </View>
+
+      <Card />
     </SafeAreaView>
   );
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Flatlist} from 'react-native';
+
+const Card = () => {
+  return (
+    <View>
+      <Text>Some Text</Text>
+    </View>
+  );
+};
+
+export default Card;
